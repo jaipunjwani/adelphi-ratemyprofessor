75f3c7ef06061e3e1b003842ea6677c1aad39469
MikeVessia <mike.vessia@gmail.com>
2015-11-05 00:40:17 -0500

    retrieving professor urls correctly

--- content.js
+++ content.js
@@ -1,13 +1,15 @@
 var names = [];
 
 function getNames(){
-				// Loop through the Professors column to grab their names
-				// TODO: Find a better way to iterate over HTML columns
-				var tds = document.getElementsByTagName('td');
-				for (var i=5; i<tds.length; i+= 10) {
-								var name = tds[i].getElementsByTagName('a')[0].innerHTML;
-								names.push(name);
-				}
+  // Loop through the Professors column to grab their names
+  // TODO: Find a better way to iterate over HTML columns
+  var tds = document.getElementsByTagName('td');
+  for (var i=5; i<tds.length; i+= 10) {
+    if(typeof tds[i].getElementsByTagName('a') != 'undefined'){
+      var name = tds[i].getElementsByTagName('a')[0].textContent.toUpperCase();
+      names.push(name);
+    }
+  }
 }
 
 /**
@@ -15,16 +17,33 @@ function getNames(){
  *  
  */
 function getURL(name) {
-				var last_name = name.replace(/,.*/, "");
-				chrome.runtime.sendMessage({
-								method: 'GET',
-								action: 'xhttp',
-								url: 'http://www.ratemyprofessors.com/search.jsp',
-								data : 'queryBy=teacherName&schoolName=adelphi+university&queryoption=HEADER&query=' + last_name
-				}, function (response) {
+  var last_name = name.replace(/,.*/, "");
+  chrome.runtime.sendMessage({
+    method: 'POST',
+    action: 'xhttp',
+    url: 'http://www.ratemyprofessors.com/search.jsp',
+    data : 'queryBy=teacherName&schoolName=adelphi+university&queryoption=HEADER&query=' + last_name + '&facetSearch=true'
+  }, function (response) {
 
-								console.log(response);
-				});
+    //console.log(response);
+    var lis = document.createElement( 'html' );
+    lis.innerHTML = response;
+
+    lis = lis.getElementsByClassName('listing PROFESSOR');
+    //console.log(lis);
+
+    for( var i = 0; i < lis.length; i++){
+      var rmp_name = lis[i].getElementsByClassName('main')[0].innerText.toUpperCase();
+      //console.log(rmp_name);
+      //console.log(name);
+      //console.log(rmp_name.indexOf(name));
+      if( rmp_name.indexOf(name) > -1 ) {
+        console.log(lis[i].getElementsByTagName('a')[0].getAttribute('href'));
+      }
+    }
+
+
+  });
 }
 
 
@@ -35,5 +54,6 @@ function getURL(name) {
 getNames();
 console.log(names);
 
-for(name in names)
-				getURL(name);
+for(var i = 0; i < names.length; i++){
+  getURL(names[i]);
+}
